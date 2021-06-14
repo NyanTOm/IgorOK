fd0d5c34bdf852b31666588c32114b9a4ddb5445
NyanTOm <83422793+NyanTOm@users.noreply.github.com>
2021-06-15 01:31:58 +0300

    message

--- sendmail.php
+++ sendmail.php
@@ -2,7 +2,7 @@
   use PHPMailer\PHPMailer\PHPMailer;
   use PHPMailer\PHPMailer\Exception;
 
-  require 'phpmailer/src/Exeption.php';
+  require 'phpmailer/src/Exception.php';
   require 'phpmailer/src/PHPMailer.php';
 
   $mail = new PHPMailer(true);
