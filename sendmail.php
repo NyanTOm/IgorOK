2d2bb6b27c192c7332d263ce4950634d3909a281
NyanTOm <83422793+NyanTOm@users.noreply.github.com>
2021-06-14 23:33:21 +0300

    Yes

---
+++ sendmail.php
@@ -0,0 +1,45 @@
+<?php
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\Exeption;
+
+  require 'phpmailer/src/Exeption.php';
+  require 'phpmailer/src/PHPMailer.php';
+
+  $mail = new PHPMailer(true);
+  $mail->CharSet = 'UTF-8';
+  $mail->setLanguage('ru', 'phpmailer/language/');
+  $mail->IsHTML(true);
+
+  //От кого письмо
+  $mail->setForm('info@fls.guru', 'Фрилансер по жизни');
+  //Кому отправлять
+  $mail->addAddress('mirors227@gmail.com');
+  //Тема письма
+  $mail->Subject = 'Привет! "Это фрилансер по жизни"';
+
+  //Тело письма
+  $body = '<h1>Встречайте супер письмо</h1>';
+
+  if (trim(!empty(&_POST['name']))) {
+    $body.='<p><string>Имя:</strong> '.&_POST['name'].'</p>';
+  }
+  if (trim(!empty(&_POST['email']))) {
+    $body.='<p><string>E-mail:</strong> '.&_POST['email'].'</p>';
+  }
+  if (trim(!empty(&_POST['message']))) {
+    $body.='<p><string>Сообщение:</strong> '.&_POST['message'].'</p>';
+  }
+
+  $mail->Body = $body;
+
+  //Отправляем
+
+  if (!$mail->send()) {
+    $message = 'Ошибка';
+  }else {
+      $message = 'Данные отправлены!';
+  }
+  $response = ['message' => $message];
+  header('Content-type: application/json');
+  echo json_endcode($response);
+  ?>
